0857aed25d5484440a406e42193a9a4ca018915e
Sean Behan <codebam@riseup.net>
2024-05-30 18:16:20 -0400

    fix vitest

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,8 +1,6 @@
-import { describe, it, expect } from "vitest";
+import { expect, test } from "vitest";
 import Main from "../src/index.js";
 
-describe("main", () => {
-	it("hello", () => {
-		expect(Main.hello).toBe("world");
-	});
+test("main.hello equals world", () => {
+	expect(Main.hello).toBe("world");
 });
