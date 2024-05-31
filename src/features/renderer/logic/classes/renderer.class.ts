07a72a2f0ebe51166eda5e3adb99c11c64035b4e
makano <makanobush@gmail.com>
2024-05-31 18:09:32 +0300

    Renderer Render function

--- src/features/renderer/logic/classes/renderer.class.ts
+++ src/features/renderer/logic/classes/renderer.class.ts
@@ -3,23 +3,58 @@ import size from 'window-size';
 class Renderer {
 	width = 0;
 	height = 0;
+	private state: string[] = [];
 
 	constructor() {
 		this.width = this.getWidth();
 		this.height = this.getHeight();
+		this.initializeState();
 	}
 
-	clear = () => {
-		Array(this.height)
-			.fill(0)
-			.forEach(() => {
-				process.stdout.write(' '.repeat(this.width));
-			});
-	};
+	initializeState() {
+		this.state = Array(this.height * this.width).fill(' ');
+	}
+
+	save(char: string, index: number = -1) {
+		if (index < 0) {
+			this.state.push(char);
+		} else {
+			this.state[index] = char;
+		}
+	}
+
+	write(char: string, at?: number) {
+		if (at !== undefined) {
+			this.save(char, at);
+		} else {
+			this.save(char);
+		}
+		this.render();
+	}
+
+	clearScreen() {
+		this.initializeState();
+		this.render();
+	}
+
+	clear() {
+		process.stdout.write('\x1b[2J'); // Clear the entire screen
+		process.stdout.write('\x1b[0;0H'); // Move the cursor to the top-left corner
+	}
+
+	render() {
+		this.clear();
+		let output = '';
+		for (let i = 0; i < this.height; i++) {
+			output += this.state.slice(i * this.width, (i + 1) * this.width).join('') + '\n';
+		}
+		process.stdout.write(output.trim());
+	}
 
 	getHeight = () => {
 		return size.height;
 	};
+
 	getWidth = () => {
 		return size.width;
 	};
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import Renderer from './features/renderer/logic/classes/renderer.class.js';
 
 const renderer = new Renderer();
-renderer.clear();
+renderer.clearScreen();
+
+const writing = 'Hello';
+
+writing.split('').forEach((char, index) => {
+  setTimeout(() => renderer.write(char, index), index * 100);
+});
 
 export default { Renderer };
