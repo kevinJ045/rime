3c4052e9ede676aba4a9b52b5fe9ccd21bbacdac
Sean Behan <codebam@riseup.net>
2024-05-31 11:35:55 -0400

    fix linting issue

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,9 @@ renderer.clearScreen();
 const writing = 'Hello';
 
 writing.split('').forEach((char, index) => {
-  setTimeout(() => renderer.write(char, index + (renderer.getWidth() * 2)), index * 100);
+  setTimeout(() => {
+    renderer.write(char, index + renderer.getWidth() * 2);
+  }, index * 100);
 });
 
 export default { Renderer };
